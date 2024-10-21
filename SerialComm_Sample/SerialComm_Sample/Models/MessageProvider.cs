887b1e36c9734a656d795b14fee184e04a336f13
yoshiosan <i.am.a.passing.kamen.rider@gmail.com>
2024-10-22 00:04:22 +0900

    ソフトウェア作成完了

---
+++ SerialComm_Sample/SerialComm_Sample/Models/MessageProvider.cs
@@ -0,0 +1,71 @@
+﻿using Prism.Mvvm;
+using System;
+using System.IO.Ports;
+using System.Text;
+using System.Windows;
+
+namespace SerialComm_Sample.Models
+{
+    public class MessageProvider : BindableBase
+    {
+        private SerialPort m_SerialPort = null;
+
+        private string m_RecvMessage = null;
+        public string RecvMessage
+        {
+            get { return m_RecvMessage; }
+            set { SetProperty( ref m_RecvMessage, value ); }
+        }
+
+        public MessageProvider( )
+        {
+            m_SerialPort = new SerialPort( );
+            m_SerialPort.PortName = "COM3";
+            m_SerialPort.BaudRate = 9600;
+            m_SerialPort.DataBits = 8;
+            m_SerialPort.Parity = Parity.None;
+            m_SerialPort.Encoding = Encoding.UTF8;
+            m_SerialPort.WriteTimeout = 5000;
+            m_SerialPort.ReadTimeout = 5000;
+            m_SerialPort.DtrEnable = true;
+
+            m_SerialPort.DataReceived += ( sender, e ) =>
+            {
+                try
+                {
+                    string message = m_SerialPort.ReadExisting();
+
+                    Application.Current.Dispatcher.Invoke( ( ) =>
+                    {
+                        RecvMessage = message;
+                    } );
+                } catch ( Exception ex )
+                {
+                    MessageBox.Show( ex.Message );
+                }
+            };
+        }
+
+        public void Connect( )
+        {
+            try
+            {
+                m_SerialPort.Open( );
+            } catch ( Exception ex )
+            {
+                MessageBox.Show( ex.Message );
+            }
+        }
+
+        public void Disconnect( )
+        {
+            try
+            {
+                m_SerialPort.Close( );
+            } catch ( Exception ex )
+            {
+                MessageBox.Show( ex.Message );
+            }
+        }
+    }
+}
--- SerialComm_Sample/SerialComm_Sample/ViewModels/MainWindowViewModel.cs
+++ SerialComm_Sample/SerialComm_Sample/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,38 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using SerialComm_Sample.Models;
 
 namespace SerialComm_Sample.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
-        public string Title
+        private MessageProvider m_MessageProvider = null;
+
+        public DelegateCommand ConnectCommand { get; }
+        public DelegateCommand DisconnectCommand { get; }
+
+        public string RecvMessage
         {
-            get { return _title; }
-            set { SetProperty( ref _title, value ); }
+            get { return m_MessageProvider.RecvMessage; }
+            set { m_MessageProvider.RecvMessage = value; }
         }
 
         public MainWindowViewModel( )
         {
+            m_MessageProvider = new MessageProvider( );
+            m_MessageProvider.PropertyChanged += ( sender, e ) => RaisePropertyChanged( e.PropertyName );
+            ConnectCommand = new DelegateCommand( Connect_Click );
+            DisconnectCommand = new DelegateCommand( Disconnect_Click );
+        }
 
+        private void Connect_Click( )
+        {
+            m_MessageProvider.Connect( );
+        }
+
+        private void Disconnect_Click( )
+        {
+            m_MessageProvider.Disconnect( );
         }
     }
 }
